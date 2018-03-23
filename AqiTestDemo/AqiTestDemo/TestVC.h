08d803cdd5809f29a65f54836028c4f07f947926
hsf <hsf_ios@sina.com>
2018-03-23 14:35:05 +0800

    添加demo

---
+++ AqiTestDemo/AqiTestDemo/TestVC.h
@@ -0,0 +1,13 @@
+//
+//  TestVC.h
+//  AqiTestDemo
+//
+//  Created by 黄山锋 on 2018/3/23.
+//  Copyright © 2018年 黄山锋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestVC : UIViewController
+
+@end
---
+++ AqiTestDemo/AqiTestDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AqiTestDemo
+//
+//  Created by 黄山锋 on 2018/3/23.
+//  Copyright © 2018年 黄山锋. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
